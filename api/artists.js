6aa2864f1096cbbcf9b9bc0ef3cffe978478a018
William Blake <gwilliamblake@gmail.com>
2019-12-10 16:23:16 -0600

    added delete route

--- api/artists.js
+++ api/artists.js
@@ -106,4 +106,23 @@ artistsRouter.put('/:artistId', (req, res, next) => {
   });
 });
 
+artistsRouter.delete('/:artistId', (req, res, next) => {
+  const sql = 'UPDATE Artist SET is_currently_employed = 0 WHERE Artist.id = $artistId';
+  const values = {$artistId: req.params.artistId};
+
+  db.run(sql, values, (error) => {
+    if (error) {
+      next(error);
+    } else {
+      db.get(`SELECT * FROM Artist WHERE Artist.id = ${req.params.artistId}`, (error, artist) => {
+        if (error) {
+          next(error);
+        } else {
+          res.status(200).json({artist: artist})
+        }
+      });
+    }
+  });
+});
+
 module.exports = artistsRouter;
